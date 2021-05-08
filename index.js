605e39532326429a5366182fbbe3838db5910b97
Rosen <rosen@topvine.co>
2021-05-08 19:26:07 +0300

    Express server was created and Redis was installed

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const Blockchain = require('./blockchain');
+
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(express.json());
+
+app.get('/api/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/blocks');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening at localhost:${PORT}`)
+})
